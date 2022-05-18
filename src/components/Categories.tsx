b61bb7a71f22552de9130a30dfe5527c0c584ccb
damirqa <damirshaniyazov@gmail.com>
2022-05-18 09:18:48 +0500

    set active category

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
   return (
     <div className="categories">
       <ul>
-        <li>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((value, index) => (
+          <li
+            key={index}
+            onClick={() => setActiveCategory(index)}
+            className={activeCategory === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
