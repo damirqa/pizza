a1a78c3a6ab1277e510860d0b8518edb454778e2
damirqa <damirshaniyazov@gmail.com>
2022-05-24 01:11:29 +0500

    added not found component

---
+++ src/components/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Root, Title, Smile, Description } from "./styles";
+
+const NotFound = () => {
+  return (
+    <Root>
+      <Title>
+        <Smile>😔</Smile>
+        <br />
+        Ничего не найдено
+      </Title>
+      <Description>К сожалению данная страница отсутствует</Description>
+    </Root>
+  );
+};
+
+export default NotFound;
---
+++ src/components/NotFound/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Root = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  padding: 150px 100px;
+`;
+
+export const Title = styled.h1`
+  font-size: 32px;
+`;
+
+export const Smile = styled.span`
+  font-size: 64px;
+`;
+
+export const Description = styled.div`
+  font-size: 18px;
+`;
