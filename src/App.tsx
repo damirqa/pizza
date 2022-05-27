a78149b00ec91655e7e20c65ca182b80977fc0f8
damirqa <damirshaniyazov@gmail.com>
2022-05-27 19:23:46 +0500

    added cart and not found comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import PizzaBlockSkeleton from "./components/skeletons/PizzaBlockSkeleton";
-// import pizzas from "./assets/pizzas.json";
-
-interface IPizza {
-  id: number;
-  imageUrl: string;
-  title: string;
-  types: number[];
-  sizes: number[];
-  price: number;
-  category: number;
-  rating: number;
-}
+import Home from "./pages/Home";
 
 function App() {
-  const [pizzas, setPizzas] = useState<IPizza[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://628706c7e9494df61b30ccdf.mockapi.io/pizzas")
-      .then((res) => res.json())
-      .then((arr) => {
-        setPizzas(arr);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => (
-                  <PizzaBlockSkeleton key={index} />
-                ))
-              : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
