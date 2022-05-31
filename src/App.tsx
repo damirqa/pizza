225b8e65bb096c8379f1622681587973cad00fa4
damirqa <damirshaniyazov@gmail.com>
2022-06-01 00:05:28 +0500

    added cart page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Cart />
         </div>
       </div>
     </div>
